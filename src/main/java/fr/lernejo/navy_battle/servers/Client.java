86921be6ba5860372ef1fed40929e54bd2ef3fec
Lairopw <91191949+Lairopw@users.noreply.github.com>
2023-01-13 19:53:25 +0100

    rectification

--- src/main/java/fr/lernejo/navy_battle/servers/Client.java
+++ src/main/java/fr/lernejo/navy_battle/servers/Client.java
@@ -27,19 +27,5 @@ public class Client {
 
         HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("start request send from port " + this.port + " to " + url);
-
-    }
-    public void CreateFireRequest(String url, String cell) throws IOException, InterruptedException {
-        HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create(url + "/api/game/fire?cell=" + cell))
-            .setHeader("accept", "application/json")
-            .setHeader( "Content-Type", "application/json" )
-            .build();
-
-        HttpResponse<String> response = this.client.send(request, HttpResponse.BodyHandlers.ofString());
-        if(response.body().contains("false")){
-            System.out.println("game end");
-            System.exit(1);
-        }
     }
 }
